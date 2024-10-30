31263b9d8327b1dcd9c27eda5e448d9b835118b4
VIKAAS <87924920+vikaas-0304@users.noreply.github.com>
2024-10-30 21:53:30 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,105 @@
+const logotext = "VIKAAS";
+const meta = {
+    title: "VIKAAS",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m VIKAAS",
+    animated: {
+        first: "I love Hacking",
+        second: "I crack codes, not jokes",
+        third: "Cybersecurity is my passion",
+    },
+    description: "An emerging talent in the field of cybersecurity focused on penetration testing and ethical hacking. Eager to learn and collaborate with others to strengthen defenses against cyber threats",
+    your_img_url: "https://c0.wallpaperflare.com/preview/456/205/254/adult-back-view-black-city-thumbnail.jpg",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I'm Vikaas, a passionate individual focused on penetration testing and ethical hacking. With a strong commitment to expanding my skills, I am eager to contribute to the cybersecurity field and engage with like-minded professionals.",
+};
+const worktimeline = [{
+        jobtitle: "Cyber Security",
+        where: "Intern",
+        date: "October, 2024",
+    },
+    {
+        jobtitle: "Full Stack Web Development",
+        where: "Intern",
+        date: "August, 2024",
+    },
+    {
+        jobtitle: "Python",
+        where: "Intern",
+        date: "April, 2024",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "C/C++",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Penetration Testing Tools",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Vulnerability Assessments",
+        description: "Identify and analyze potential security vulnerabilities in systems and applications to enhance overall security posture and mitigate risks.",
+    },
+    {
+        title: "Web Application Testing",
+        description: "Perform in-depth testing of web applications to discover and address security flaws, ensuring robust protection against threats like SQL injection and cross-site scripting.",
+    },
+    {
+        title: "Password Cracking",
+        description: "Evaluate the strength of passwords used in systems by attempting to crack them, providing insights and recommendations to enhance password security measures.",
+    },
+];
+
+const dataportfolio = [{
+    img: "https://cronuts.digital/wp-content/uploads/2023/12/Captura-de-pantalla-2023-12-05-124236.png",
+    description: "Device Driver Scanner",
+    link: "https://github.com/vikaas-0304/DeviceDriverScanner.git",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vikaas0304muthu@gmail.com",
+    YOUR_FONE: "99437 19584",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/vikaas-0304",
+    linkedin: "https://www.linkedin.com/in/vikaas0304in",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
